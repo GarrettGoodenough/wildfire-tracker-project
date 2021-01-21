c4246a26e6463b20a7e30a0d3e1cffbe4cb5f528
GarrettGoodenough <69942588+GarrettGoodenough@users.noreply.github.com>
2021-01-20 23:00:32 -0800

    Wildfire-Tracker Completed

---
+++ src/components/Loader.js
@@ -0,0 +1,11 @@
+import loader from './loader.gif'
+
+const Loader = () => {
+    return (
+        <div className="loader">
+            <h1>Fetching Data</h1>
+        </div>
+    )
+}
+
+export default Loader
